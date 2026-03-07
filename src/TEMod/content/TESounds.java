7142b08f6be8dc820f2e1566b47b2c28b0709672
m1cxzfw3q <mcxzfwq@outlook.com>
2026-03-07 16:38:42 +0800

    update

--- src/TEMod/content/TESounds.java
+++ src/TEMod/content/TESounds.java
@@ -1,27 +1,30 @@
 package TEMod.content;
 
-import arc.Core;
-import arc.assets.AssetDescriptor;
-import arc.assets.loaders.SoundLoader;
 import arc.audio.Sound;
 import arc.files.Fi;
 import arc.struct.ObjectMap;
 import arc.struct.Seq;
+import arc.util.Log;
 import mindustry.Vars;
+import mindustry.gen.Sounds;
 
 public class TESounds {
     private static final ObjectMap<String, Sound> sounds = new ObjectMap<>();
     public static void load() {
-        Seq<Fi> file = Vars.tree.get("sounds/").findAll(s -> s.name().contains(".ogg") || s.name().contains(".mp3"));
-        for (Fi fi : file) {
-            Sound sound = new Sound();
-            AssetDescriptor<?> desc = Core.assets.load(fi.path(), Sound.class, new SoundLoader.SoundParameter(sound));
-            desc.errored = Throwable::printStackTrace;
-            sounds.put(fi.name(), sound);
+        Fi path = Vars.tree.get("sounds/");
+        if (path.exists() && path.isDirectory()){
+            Seq<Fi> files = path.findAll();
+            for (Fi fi : files) {
+                sounds.put(fi.name(), Vars.tree.loadSound(fi.name()));
+            }
         }
     }
 
     public static Sound get(String soundName) {
-        return sounds.get(soundName);
+        if (sounds.containsKey(soundName)) return sounds.get(soundName);
+        else {
+            Log.warn("[TEMod] Sound " + soundName + " not found.");
+            return Sounds.none;
+        }
     }
 }
