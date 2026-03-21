d57760d13033abf591a36c626e97bd44ca3001b5
m1cxzfw3q <mcxzfwq@outlook.com>
2026-03-22 03:13:48 +0800

    update

--- src/TEMod/content/TEBlocks.java
+++ src/TEMod/content/TEBlocks.java
@@ -1490,7 +1490,7 @@ public class TEBlocks {
             health = 120;
         }};
 
-        starshipAssemblerExpandInputSlot = new TEUnitAssemblerModule("starship-assembler-expand-input-slot") {{
+        starshipAssemblerExpandInputSlot = new TEUnitAssemblerModule("starship-assembler-expand-input-module") {{
             requirements(Category.units, with(TEItems.ultimateAgreement, 1, TEItems.ultimateChip, 50));
             consumePower(100000 / 60f);
             tier = 0;
